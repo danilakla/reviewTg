85336736d9e61b9eef355445791f6a247e98dab9
Danila <67117334+danilakla@users.noreply.github.com>
2025-06-18 16:22:31 +0300

    update fix

--- src/main/java/com/example/telegramreminderbot/config/TelegramBotConfig.java
+++ src/main/java/com/example/telegramreminderbot/config/TelegramBotConfig.java
@@ -1,6 +1,7 @@
 package com.example.telegramreminderbot.config;
 
 import com.example.telegramreminderbot.telegram.TelegramApiClient;
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,15 +9,31 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+
 @Configuration
 @RequiredArgsConstructor
 public class TelegramBotConfig {
     private final TelegramApiClient telegramApiClient;
+    private TelegramBotsApi telegramBotsApi;
 
     @Bean
     public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
-        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
-        api.registerBot(telegramApiClient);
-        return api;
+        telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(telegramApiClient);
+        return telegramBotsApi;
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        if (telegramBotsApi != null) {
+            try {
+                // Stop the bot session
+                telegramApiClient.clearWebhook();
+                // The session will be automatically cleaned up by Spring
+            } catch (TelegramApiException e) {
+                // Log error but don't throw as this is during shutdown
+                e.printStackTrace();
+            }
+        }
     }
 } 
